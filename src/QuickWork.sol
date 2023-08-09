49ebf93991e5d77a4fda4dd09e78d20bc7bf4b56
cristianizzo <cristianizzo@me.com>
2023-08-09 20:41:30 +0200

    init

--- src/QuickWork.sol
+++ src/QuickWork.sol
@@ -1,206 +1,108 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
 /**
- * @title WorkContract
- * @dev This contract allows a payer to manage tasks and their associated payments.
+ * @title QuickWork
+ * @dev This contract allows a manager to manage tasks and their associated payments using native ether.
  */
 contract QuickWork is ReentrancyGuard {
-    address public payer;  // Address of the payer
-    address public tokenAddress;  // Address of the ERC20 token (e.g., USDC)
-    uint256 public unallocatedFunds = 0;  // Funds that are not allocated to any task
+  address public manager;  // Address of the manager
 
-    // Enumeration for task statuses
-    enum TaskStatus {Pending, Approved, Rejected, Disputed}
+  // Struct for task details
+  struct Task {
+    uint256 id;  // Unique identifier for the task
+    uint256 amount;  // Amount allocated for the task in wei
+    address payable payerAddress;  // Address of the payer for the task
+    address payable payeeAddress;  // Address of the payee for the task
+    address approverAddress;  // Address of the approver
+  }
 
-    // Struct for payee details
-    struct Payee {
-        address payable payeeAddress;  // Address of the payee
-        uint256 balance;  // Balance of the payee
-    }
+  // Mapping to store balances by address
+  mapping(address => uint256) public balances;
+  // Mapping to store task details by task ID
+  mapping(uint256 => Task) public tasks;
 
-    // Struct for task details
-    struct Task {
-        uint256 id;  // Unique identifier for the task
-        uint256 amount;  // Amount allocated for the task
-        address payeeAddress;  // Address of the payee for the task
-        TaskStatus status;  // Status of the task
-    }
+  // Events
+  event TaskAdded(uint256 indexed taskId, address indexed payeeAddress, uint256 amount, address indexed approverAddress);
+  event TaskApproved(uint256 indexed taskId, address indexed payeeAddress);
+  event TaskRejected(uint256 indexed taskId, address indexed payerAddress);
+  event FundsWithdrawn(address indexed addressWithdrawn, uint256 amount);
 
-    // Struct for task update requests
-    struct TaskUpdateRequest {
-        uint256 newAmount;  // Proposed new amount for the task
-        bool payerApproved;  // Whether the payer has approved the update
-        bool payeeApproved;  // Whether the payee has approved the update
-    }
+  // Modifiers
+  modifier onlyManager() {
+    require(msg.sender == manager, "Only the manager can call this function");
+    _;
+  }
 
-    // Mapping to store update requests by task ID
-    mapping(uint256 => TaskUpdateRequest) public updateRequests;
-    // Mapping to store payee details by address
-    mapping(address => Payee) public payees;
-    // Mapping to store task details by task ID
-    mapping(uint256 => Task) public tasks;
+  modifier onlyApprover(uint256 taskId) {
+    require(msg.sender == tasks[taskId].approverAddress, "Only the designated approver can call this function");
+    _;
+  }
 
-    // Events
-    event PayeeAdded(address indexed payeeAddress);
-    event TaskAdded(uint256 indexed taskId, address indexed payeeAddress, uint256 amount);
-    event TaskApproved(uint256 indexed taskId, address indexed payeeAddress);
-    event TaskRejected(uint256 indexed taskId, address indexed payeeAddress);
-    event FundsWithdrawn(address indexed payeeAddress, uint256 amount);
-    event PayerFundsRetrieved(address indexed payer, uint256 amount);
-    event TaskUpdateProposed(uint256 indexed taskId, uint256 newAmount, address proposer);
-    event TaskUpdateApproved(uint256 indexed taskId, uint256 newAmount, address approver);
+  /**
+   * @dev Constructor to set the initial manager.
+     * @param _manager Address of the manager.
+     */
+  constructor(address _manager) {
+    manager = _manager;
+  }
 
-    // Modifiers
-    modifier onlyPayer() {
-        require(msg.sender == payer, "Only the payer can call this function");
-        _;
-    }
+  /**
+   * @dev Function to add a new task.
+     * @param _taskId Unique identifier for the task.
+     * @param _amount Amount allocated for the task in wei.
+     * @param _payerAddress Address of the payer for the task.
+     * @param _payeeAddress Address of the payee for the task.
+     * @param _approverAddress Address of the approver for the task.
+     */
+  function addTask(uint256 _taskId, uint256 _amount, address payable _payerAddress, address payable _payeeAddress, address _approverAddress) public payable {
+    require(_payerAddress != address(0) && _payeeAddress != address(0), "Payer and Payee addresses must not be zero");
+    require(_payerAddress != _payeeAddress, "Payer and Payee addresses must be distinct");
+    require(tasks[_taskId].id == 0, "Task ID already exists");
+    require(_amount > 0, "Amount should be greater than 0");
+    require(msg.value == _amount, "Sent ether value mismatch");
 
-    modifier onlyPayee(address _payeeAddress) {
-        require(msg.sender == _payeeAddress, "Only the payee can call this function");
-        _;
-    }
+    tasks[_taskId] = Task(_taskId, _amount, _payerAddress, _payeeAddress, _approverAddress);
+    emit TaskAdded(_taskId, _payeeAddress, _amount, _approverAddress);
+  }
 
-    /**
-     * @dev Constructor to set the initial payer and token address.
-   * @param _payer Address of the payer.
-   * @param _tokenAddress Address of the ERC20 token.
-   */
-    constructor(address _payer, address _tokenAddress) {
-        payer = _payer;
-        tokenAddress = _tokenAddress;
-    }
+  /**
+   * @dev Function for the approver to approve a task.
+     * @param _taskId Unique identifier for the task.
+     */
+  function approveTask(uint256 _taskId) public onlyApprover(_taskId) {
+    require(tasks[_taskId].id != 0, "Task ID does not exist");
+    require(tasks[_taskId].amount != 0, "Task has already been approved or rejected");
 
-    /**
-     * @dev Function to add a new payee.
-   * @param _payeeAddress Address of the new payee.
-   */
-    function addPayee(address payable _payeeAddress) public onlyPayer {
-        require(payees[_payeeAddress].payeeAddress == address(0), "Payee already exists");
-        payees[_payeeAddress] = Payee(_payeeAddress, 0);
-        emit PayeeAdded(_payeeAddress);
-    }
+    balances[tasks[_taskId].payeeAddress] += tasks[_taskId].amount;
+    tasks[_taskId].amount = 0;
+    emit TaskApproved(_taskId, tasks[_taskId].payeeAddress);
+  }
 
-    /**
-     * @dev Function to add a new task.
-   * @param _taskId Unique identifier for the task.
-   * @param _amount Amount allocated for the task.
-   * @param _payeeAddress Address of the payee for the task.
-   */
-    function addTask(uint256 _taskId, uint256 _amount, address _payeeAddress) public onlyPayer {
-        require(tasks[_taskId].id == 0, "Task ID already exists");
-        require(payees[_payeeAddress].payeeAddress != address(0), "Payee does not exist");
-        require(_amount > 0, "Amount should be greater than 0");
-        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), _amount), "Transfer of tokens failed");
+  /**
+   * @dev Function for the approver to reject a task.
+     * @param _taskId Unique identifier for the task.
+     */
+  function rejectTask(uint256 _taskId) public onlyApprover(_taskId) {
+    require(tasks[_taskId].id != 0, "Task ID does not exist");
+    require(tasks[_taskId].amount != 0, "Task has already been approved or rejected");
 
-        tasks[_taskId] = Task(_taskId, _amount, _payeeAddress, TaskStatus.Pending);
-        emit TaskAdded(_taskId, _payeeAddress, _amount);
-    }
+    balances[tasks[_taskId].payerAddress] += tasks[_taskId].amount;
+    tasks[_taskId].amount = 0;
+    emit TaskRejected(_taskId, tasks[_taskId].payeeAddress);
+  }
 
-    /**
-     * @dev Function for the payer to approve a task.
-   * @param _taskId Unique identifier for the task.
-   * @param _payeeAddress Address of the payee for the task.
-   */
-    function approveTask(uint256 _taskId, address _payeeAddress) public onlyPayer {
-        require(tasks[_taskId].id != 0, "Task ID does not exist");
-        require(tasks[_taskId].payeeAddress == _payeeAddress, "Payee address mismatch");
-        require(tasks[_taskId].status == TaskStatus.Pending, "Task is not in a pending state");
-        require(tasks[_taskId].status != TaskStatus.Disputed, "Task is currently under dispute");
+  /**
+   * @dev Function for anyone to withdraw their funds.
+     * @param _amount Amount to withdraw in wei.
+     */
+  function withdraw(uint256 _amount) public nonReentrant {
+    require(balances[msg.sender] >= _amount, "Insufficient balance");
 
-        payees[_payeeAddress].balance += tasks[_taskId].amount;
-        tasks[_taskId].status = TaskStatus.Approved;
-        emit TaskApproved(_taskId, _payeeAddress);
-    }
-
-    /**
-     * @dev Function for the payee to reject a task.
-   * @param _taskId Unique identifier for the task.
-   */
-    function rejectTask(uint256 _taskId) public onlyPayee(msg.sender) {
-        require(tasks[_taskId].id != 0, "Task ID does not exist");
-        require(tasks[_taskId].status == TaskStatus.Pending, "Task is not in a pending state");
-        require(tasks[_taskId].status != TaskStatus.Disputed, "Task is currently under dispute");
-
-        tasks[_taskId].status = TaskStatus.Rejected;
-        unallocatedFunds += tasks[_taskId].amount;
-        emit TaskRejected(_taskId, msg.sender);
-    }
-
-    /**
-     * @dev Function for the payer to withdraw unallocated funds.
-   * @param _amount Amount to withdraw.
-   */
-    function withdrawUnallocatedFunds(uint256 _amount) public nonReentrant onlyPayer {
-        require(unallocatedFunds >= _amount, "Insufficient balance");
-
-        unallocatedFunds -= _amount;
-        IERC20(tokenAddress).transfer(payer, _amount);
-        emit PayerFundsRetrieved(payer, _amount);
-    }
-
-    /**
-     * @dev Function for the payee to withdraw their funds.
-   * @param _amount Amount to withdraw.
-   */
-    function withdrawFunds(uint256 _amount) public nonReentrant onlyPayee(msg.sender) {
-        require(payees[msg.sender].balance >= _amount, "Insufficient balance");
-
-        payees[msg.sender].balance -= _amount;
-        IERC20(tokenAddress).transfer(msg.sender, _amount);
-        emit FundsWithdrawn(msg.sender, _amount);
-    }
-
-    /**
-     * @dev Function to propose an update to a task's amount.
-   * @param _taskId Unique identifier for the task.
-   * @param _newAmount Proposed new amount for the task.
-   */
-    function proposeTaskUpdate(uint256 _taskId, uint256 _newAmount) public nonReentrant {
-        require(tasks[_taskId].id != 0, "Task ID does not exist");
-        require(tasks[_taskId].status == TaskStatus.Pending, "Task is not in a pending state");
-        require(_newAmount > 0 && _newAmount != tasks[_taskId].amount, "New amount should be greater than 0 and different from the current amount");
-        require(msg.sender == payer || msg.sender == tasks[_taskId].payeeAddress, "Only payer or payee can propose an update");
-        require(updateRequests[_taskId].newAmount == 0, "Update for this task is already proposed");
-
-        updateRequests[_taskId] = TaskUpdateRequest({
-        newAmount: _newAmount,
-        payerApproved: msg.sender == payer,
-        payeeApproved: msg.sender == tasks[_taskId].payeeAddress
-        });
-
-        emit TaskUpdateProposed(_taskId, _newAmount, msg.sender);
-    }
-
-    /**
-     * @dev Function to approve a proposed task update.
-   * @param _taskId Unique identifier for the task.
-   */
-    function approveTaskUpdate(uint256 _taskId) public nonReentrant {
-        require(tasks[_taskId].id != 0, "Task ID does not exist");
-        require(tasks[_taskId].status == TaskStatus.Pending, "Task is not in a pending state");
-        require(msg.sender == payer || msg.sender == tasks[_taskId].payeeAddress, "Only payer or payee can approve an update");
-        require(updateRequests[_taskId].newAmount != 0, "No update proposed for this task");
-
-        if(msg.sender == payer) {
-            require(!updateRequests[_taskId].payerApproved, "Payer has already approved this update");
-            updateRequests[_taskId].payerApproved = true;
-        } else {
-            require(!updateRequests[_taskId].payeeApproved, "Payee has already approved this update");
-            updateRequests[_taskId].payeeApproved = true;
-        }
-
-        emit TaskUpdateApproved(_taskId, updateRequests[_taskId].newAmount, msg.sender);
-
-        // If both parties have approved, apply the update
-        if(updateRequests[_taskId].payerApproved && updateRequests[_taskId].payeeApproved) {
-            tasks[_taskId].amount = updateRequests[_taskId].newAmount;
-            // Set the update request's newAmount to its default value (0) instead of using delete
-            updateRequests[_taskId].newAmount = 0;
-        }
-    }
+    balances[msg.sender] -= _amount;
+    payable(msg.sender).transfer(_amount);
+    emit FundsWithdrawn(msg.sender, _amount);
+  }
 }
--- test/QuickWork.t.sol
+++ test/QuickWork.t.sol
@@ -1,24 +1,114 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {QuickWork} from "../src/QuickWork.sol";
+import "openzeppelin-contracts/lib/forge-std/src/console.sol";
 
 contract QuickWorkTest is Test {
-    QuickWork public quickWork;
+  QuickWork public quickWork;
+  address payable manager;
+  address payable payer;
+  address payable payee;
+  address payable approver;
 
-    function setUp() public {
-        counter = new QuickWork();
-        counter.setNumber(0);
-    }
+  function setUp() public {
+    manager = payable(address(0xABCD));
+    // For simplicity, we'll use the test contract as the manager
+    payer = payable(address(0xABC));
+    // Dummy payer address
+    payee = payable(address(0x123));
+    // Dummy payee address
+    approver = payable(address(0xDEF));
+    // Dummy approver address
+    quickWork = new QuickWork(address(this));
+  }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+  // Test adding a task
+  function testAddTask() public {
+    uint256 taskId = 1;
+    uint256 amount = 1 ether; // 1 ether for simplicity
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+    // Set the manager as the caller and send ether when adding the task
+    quickWork.addTask{value : amount}(taskId, amount, payer, payee, approver);
+
+    // Assertions
+    (uint256 returnedTaskId, uint256 returnedAmount, address returnedPayer, address returnedPayee, address returnedApprover) = quickWork.tasks(taskId);
+    assertEq(returnedTaskId, taskId);
+    assertEq(returnedAmount, amount);
+    assertEq(returnedPayer, payer);
+    assertEq(returnedPayee, payee);
+    assertEq(returnedApprover, approver);
+  }
+
+  // Test approving a task
+  function testApproveTask() public {
+    uint256 taskId = 2;
+    // Using a different taskId to avoid conflicts with the previous tests
+    uint256 amount = 1 ether;
+
+    // Set the manager as the caller and send ether when adding the task
+    quickWork.addTask{value : amount}(taskId, amount, payer, payee, approver);
+
+    // Impersonate the approver for the next call
+    vm.prank(approver);
+
+    // Approve task as the approver
+    quickWork.approveTask(taskId);
+
+    // Check that the payee's balance in QuickWork is updated
+    uint256 payeeBalanceInContract = quickWork.balances(payee);
+    assertEq(payeeBalanceInContract, amount, "Payee balance mismatch in contract after task approval");
+
+    // Impersonate the payee for the next call
+    vm.prank(payee);
+
+    // Payee withdraws the funds
+    quickWork.withdraw(amount);
+
+    // Check that the payee's balance in QuickWork is now 0
+    uint256 payeeBalanceAfterWithdrawal = quickWork.balances(payee);
+    assertEq(payeeBalanceAfterWithdrawal, 0, "Payee balance in contract should be 0 after withdrawal");
+
+    // Check that the contract's balance is also 0
+    uint256 contractBalance = address(quickWork).balance;
+    assertEq(contractBalance, 0, "Contract balance should be 0 after payee's withdrawal");
+  }
+
+  // Test rejecting a task
+  function testRejectTask() public {
+    uint256 taskId = 3; // Using a different taskId to avoid conflicts with the previous tests
+    uint256 amount = 1 ether;
+
+    // Set the manager as the caller and send ether when adding the task
+    quickWork.addTask{value : amount}(taskId, amount, payer, payee, approver);
+
+    // Impersonate the approver for the next call
+    vm.prank(approver);
+
+    // Reject task as the approver
+    quickWork.rejectTask(taskId);
+
+    // Check that the payee's balance in QuickWork is 0
+    uint256 payeeBalanceInContract = quickWork.balances(payee);
+    assertEq(payeeBalanceInContract, 0, "Payee balance should remain 0 after task rejection");
+
+    // Check that the payer's balance in QuickWork is updated correctly
+    uint256 payerBalanceInContract = quickWork.balances(payer);
+    assertEq(payerBalanceInContract, amount, "Payer balance mismatch in contract after task rejection");
+
+    // Impersonate the payer for the next call
+    vm.prank(payer);
+
+    // Payer withdraws the funds
+    quickWork.withdraw(amount);
+
+    // Check that the payer's balance in QuickWork is now 0
+    uint256 payerBalanceAfterWithdrawal = quickWork.balances(payer);
+    assertEq(payerBalanceAfterWithdrawal, 0, "Payer balance in contract should be 0 after withdrawal");
+
+    // Check that the contract's balance is also 0
+    uint256 contractBalance = address(quickWork).balance;
+    assertEq(contractBalance, 0, "Contract balance should be 0 after payer's withdrawal");
+  }
 }
